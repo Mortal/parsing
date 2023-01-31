f5051f60f9c04a786432f211c3b8e4e5df0d0d8a
Mathias Rav <m@git.strova.dk>
2023-01-31 22:11:35 +0100

    WIP

--- example.py
+++ example.py
@@ -1,26 +1,43 @@
 import argparse
 import re
-from parsing import iter_tokens
+import traceback
+from parsing import iter_tokens, ParsingError
 
 
 python_lexer = re.compile(
     r'''
 (?P<name>[a-zA-Z_][a-zA-Z0-9_]*)
+|(?P<number>[0-9]+)
+|(?P<comment>\#.*)
 |(?P<indent>^[ ]+)
-|(?P<op>!=|->|[][(),=:@-])
-|(?P<string>"(?:\\.|[^\\"])")
+|(?P<op>!=|[<>]=?|->|[][(),=:@.|%*/+-])
+|(?P<string>
+    "["]"(?:\\.|[^\\"]|"(?:[^"]|"[^"]))*"["]"
+    |'[']'(?:\\.|[^\\']|'(?:[^']|'[^']))*'[']'
+    |"(?:\\.|[^\\"])*"
+    |'(?:\\.|[^\\'])*'
+)
 ''', re.M | re.X)
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument("filename")
 
 
 def main() -> None:
     args = parser.parse_args()
-    filename = "parsing.py"
-    with open(filename) as fp:
+    with open(args.filename) as fp:
         contents = fp.read()
-    list(iter_tokens(python_lexer, filename, contents))
+    try:
+        list(iter_tokens(python_lexer, args.filename, contents))
+    except ParsingError as e:
+        traceback.print_exc()
+        lines = e.err.buffer.contents.splitlines()
+        print('File "%s", line %s' % (e.err.buffer.filename, e.err.pos.lineno))
+        print(lines[e.err.pos.lineno - 1])
+        print(" " * e.err.pos.column + "^" + "~" * (e.err.length - 1))
+        print('%s:%s:%s: %s' % (e.err.buffer.filename, e.err.pos.lineno, e.err.pos.column, e))
+        print(repr(not not e.err.buffer.contents[e.err.pos.index + e.err.length].strip()))
 
 
 if __name__ == "__main__":
--- parsing.py
+++ parsing.py
@@ -10,13 +10,17 @@ class Position:
     column: int
 
     def advanced(self, buf: str, i: int, j: int) -> "Position":
+        assert self.index >= 0
+        assert i < j, (i, j)
         nls = buf.count("\n", i, j)
         k = j - i
+        assert k > 0
         if not nls:
             return Position(self.index + k, self.lineno, self.column + k)
+        # print("Found %s newlines in %r" % (nls, buf[i:j]))
         nl = buf.rindex("\n", i, j)
-        column = j - nl
-        return Position(self.index + k, self.lineno + nl, column)
+        column = j - (nl + 1)
+        return Position(self.index + k, self.lineno + nls, column)
 
 
 @dataclass
@@ -61,8 +65,12 @@ class OptToken:
     length: int
 
     @property
+    def text(self) -> str:
+        return self.buffer.contents[self.pos.index : self.pos.index + self.length]
+
+    @property
     def blank(self) -> bool:
-        return not not self.buffer.contents[self.pos.index + self.length].strip()
+        return not self.text.strip()
 
     @property
     def unmatch(self) -> bool:
@@ -70,6 +78,8 @@ class OptToken:
 
     def unwrap(self) -> Token:
         if self.kind is None:
+            print(repr(self.text))
+            print(self.pos)
             raise ParsingError(ParsingErr("unexpected data while lexing", self.buffer, self.pos, self.length))
         return Token(self.kind, self.buffer, self.pos, self.length)
 
@@ -83,15 +93,15 @@ def iter_opt_tokens(pattern: re.Pattern[str], filename: str, contents: str) -> I
         i = mo.start()
         if pos.index != i:
             yield OptToken(None, buffer, pos, i - pos.index)
-            pos = pos.advanced(contents, i, pos.index)
+            pos = pos.advanced(contents, pos.index, i)
         i = mo.end()
         if pos.index != i:
             yield OptToken(kind, buffer, pos, i - pos.index)
-            pos = pos.advanced(contents, i, pos.index)
+            pos = pos.advanced(contents, pos.index, i)
     i = len(contents)
     if pos.index != i:
         yield OptToken(None, buffer, pos, i - pos.index)
-        pos = pos.advanced(contents, i, pos.index)
+        pos = pos.advanced(contents, pos.index, i)
 
 
 def unwrapped_non_blank(it: Iterable[OptToken]) -> Iterator[Token]:
@@ -102,4 +112,4 @@ def unwrapped_non_blank(it: Iterable[OptToken]) -> Iterator[Token]:
 
 
 def iter_tokens(pattern: re.Pattern[str], filename: str, contents: str) -> Iterator[Token]:
-    return unwrapped_non_blank(iter_opt_tokens(pattern, filename, contents))
+    yield from unwrapped_non_blank(iter_opt_tokens(pattern, filename, contents))
