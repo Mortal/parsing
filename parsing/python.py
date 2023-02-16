84f050debe74fc44797378ea44777b343b6d2d73
Mathias Rav <m@git.strova.dk>
2023-02-16 11:20:57 +0100

    Split example.py into parens.py and python.py

--- parsing/python.py
+++ parsing/python.py
@@ -2,8 +2,10 @@ import argparse
 import re
 import traceback
 from dataclasses import dataclass
-from parsing import iter_tokens, ParsingError, Token
 from typing import Iterator, Iterable
+
+from .tokens import iter_tokens, ParsingError, Token
+from .parens import Parenthesized
 
 
 python_lexer = re.compile(
@@ -28,47 +30,6 @@ python_lexer = re.compile(
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename")
-
-
-@dataclass
-class Parenthesized:
-    left: Token
-    contents: list["Token | Parenthesized"]
-    right: Token
-
-
-def match_parens(
-    tokens: Iterable[Token], parens: dict[str, str]
-) -> Iterator[Token | Parenthesized]:
-    paren_stack: list[tuple[str, Token, list[Token | Parenthesized]]] = []
-    for tok in tokens:
-        text = tok.text
-
-        if text in parens:
-            paren_stack.append((parens[text], tok, []))
-            continue
-
-        if text in parens.values():
-            if not paren_stack:
-                raise tok.to_error("unmatched parenthesis")
-            if paren_stack[-1][0] != text:
-                raise tok.to_error(
-                    "incorrectly matched parentheses: %s%s"
-                    % (paren_stack[-1][1].text, text)
-                )
-            paren = Parenthesized(paren_stack[-1][1], paren_stack[-1][2], tok)
-            paren_stack.pop()
-            if paren_stack:
-                paren_stack[-1][2].append(paren)
-            else:
-                yield paren
-            continue
-
-        if paren_stack:
-            paren_stack[-1][2].append(tok)
-            continue
-
-        yield tok
 
 
 @dataclass
