e73965046da18cfc12d6152cea4d62908ac2c314
Mathias Rav <m@git.strova.dk>
2025-11-01 18:09:53 +0000

    WIP

--- reaperprojects.py
+++ reaperprojects.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-import subprocess
+import sqlite3
 import traceback
 from dataclasses import dataclass
 from os import PathLike
@@ -11,109 +11,100 @@ from reaperparser import parse_reaper_project
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename")
+parser.add_argument("-o", "--output", required=True)
+
+
+def check_and_delete_existing(path: str) -> None:
+    conn = sqlite3.connect(f"file:{path}?mode=ro", uri=True)
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    names = [row[0] for row in cur.fetchall()]
+    if set(names) != {
+        "files",
+        "mediadirs",
+        "mediadir_files",
+        "projects",
+        "project_files",
+    }:
+        raise SystemExit(f"refusing to overwrite existing '{path}' with tables {names}")
+    os.unlink(path)
 
 
 def main() -> None:
     args = parser.parse_args()
+    if os.path.isfile(args.output):
+        check_and_delete_existing(args.output)
+    conn = sqlite3.connect(args.output)
+    conn.executescript("""
+    CREATE TABLE files (id INTEGER PRIMARY KEY, path TEXT UNIQUE, size INT);
+    CREATE TABLE mediadirs (id INTEGER PRIMARY KEY, path TEXT UNIQUE);
+    CREATE TABLE mediadir_files (
+        id INTEGER PRIMARY KEY,
+        mediadir INTEGER REFERENCES mediadirs(id) ON DELETE CASCADE,
+        file INTEGER REFERENCES files(id) ON DELETE CASCADE);
+    CREATE TABLE projects (id INTEGER PRIMARY KEY, path TEXT UNIQUE, mediadir INTEGER);
+    CREATE TABLE project_files (
+        id INTEGER PRIMARY KEY,
+        project INTEGER REFERENCES projects(id) ON DELETE CASCADE,
+        file INTEGER REFERENCES files(id) ON DELETE CASCADE);
+    """)
+    cur = conn.cursor()
+
     projectlist: list[ProjectLinks] = []
     if os.path.isfile(args.filename):
         projectlist.append(process(args.filename))
-    sizes: dict[PurePath, int] = {}
-    uses: dict[PurePath, list[str]] = {}
+    fileid: dict[PurePath, int] = {}
     for dirpath, dirs, files in os.walk(args.filename):
         dirs.sort()
         files.sort()
         for filename in files:
-            path = os.path.join(dirpath, filename)
-            sizes[path] = os.path.getsize(path)
+            path = PurePath(os.path.join(dirpath, filename))
+            cur.execute(
+                "INSERT INTO files (path, size) VALUES (?, ?)",
+                (str(path), os.path.getsize(path)),
+            )
+            assert cur.lastrowid
+            fileid[path] = cur.lastrowid
             ext = filename.lower()
             if ext.endswith(".rpp"):
                 project = process(path)
                 projectlist.append(project)
-            elif ext.endswith((".wav", ".mp3", ".m4a", ".flac")):
-                uses[path] = []
     projects: dict[PurePath, list[ProjectLinks]] = {}
     for project in projectlist:
-        record_path = PurePath(project.project_path).parent / PurePath(project.record_path)
+        record_path = PurePath(project.project_path).parent / PurePath(
+            project.record_path
+        )
         projects.setdefault(record_path, []).append(project)
-    owner: dict[PurePath, PurePath] = {}
-    outsideuses: dict[str, list[PurePath]] = {}
     for record_path in projects:
+        cur.execute("INSERT INTO mediadirs (path) VALUES (?)", (str(record_path),))
+        mediadirid = cur.lastrowid
+        assert mediadirid
         for dirpath, dirs, files in os.walk(record_path):
             dirs.sort()
             files.sort()
             for filename in files:
                 filepath = record_path / dirpath / filename
-                uses.setdefault(filepath, [])
-                ex = owner.setdefault(filepath, record_path)
-                if ex != record_path:
-                    raise Exception(f"overlapping media dirs: {ex} {record_path} {filepath}")
-        for project in projects[record_path]:
-            if str(record_path) == "/home/mathias/Music/Media":
-                print(project.project_path)
-            project_dir = PurePath(project.project_path).parent
-            for item in project.media_items:
-                itempath = project_dir / PurePath(item)
-                uses.setdefault(itempath, []).append(project.project_path)
-                if record_path not in itempath.parents:
-                    outsideuses.setdefault(project.project_path, []).append(itempath)
-    for project_path, itempaths in outsideuses.items():
-        print(project_path, "->", *sorted(set(owner[itempath] for itempath in itempaths)))
-    record_path_sizes: dict[PurePath, int] = {record_path: 0 for record_path in projects}
-    outside_record_path: list[tuple[PurePath, int]] = []
-    for path in uses:
-        if path not in sizes:
-            try:
-                sizes[path] = os.path.getsize(path)
-            except FileNotFoundError:
-                sizes[path] = -1
-        if sizes[path] >= 0:
-            if path in owner:
-                record_path_sizes[owner[path]] += sizes[path]
-            else:
-                outside_record_path.append((path, sizes[path]))
-    sizeinuse = 0
-    sizenotinuse = 0
-    for path in uses:
-        if uses[path]:
-            sizeinuse += sizes[path]
-        else:
-            sizenotinuse += sizes[path]
-        print("\t".join(map(str, (len(uses[path]), sizes[path], path))))
-    statbroken = 0
-    brokennames: set[str] = set()
-    for record_path in sorted(projects, key=lambda p: record_path_sizes[p]):
-        print("===========", f"{record_path_sizes[record_path] / 2**30:.2f} GB -", record_path)
+                cur.execute(
+                    "INSERT INTO mediadir_files (mediadir, file) VALUES (?, ?)",
+                    (mediadirid, fileid[filepath]),
+                )
         for project in projects[record_path]:
             project_dir = PurePath(project.project_path).parent
-            broken: list[str] = []
-            totsize = 0.0
+            cur.execute(
+                "INSERT INTO projects (path, mediadir) VALUES (?, ?)",
+                (project.project_path, mediadirid),
+            )
+            projectid = cur.lastrowid
+            assert projectid
             for item in project.media_items:
-                itempath = project_dir / PurePath(item)
-                itemsize = sizes[itempath]
-                if itemsize == -1:
-                    broken.append(item)
-                    brokennames.add(os.path.basename(item))
-                else:
-                    totsize += itemsize / len(uses[itempath])
-            if broken:
-                statbroken += 1
-                print("BROKEN", round(totsize), project.project_path, *broken)
-            else:
-                print(round(totsize), project.project_path)
-    print("===========", "Outside")
-    for path, size in outside_record_path:
-        print(size, path, *uses[path])
-
-    print(f"{statbroken} broken projects, {sizeinuse/2**30:.2f} GB used media, {sizenotinuse/2**30:.2f} GB unused media")
-
-    if brokennames:
-        findargs = ["find", args.filename]
-        for i, nam in enumerate(sorted(brokennames)):
-            if i:
-                findargs.append("-or")
-            findargs += ["-name", nam]
-        subprocess.call(findargs)
+                itempath = project_dir / item
+                cur.execute(
+                    "INSERT INTO project_files (project, file) VALUES (?, ?)",
+                    (projectid, fileid[itempath]),
+                )
+    cur.close()
+    conn.commit()
+    conn.close()
 
 
 @dataclass(frozen=True)
